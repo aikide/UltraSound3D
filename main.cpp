6ed4ba1859e43512ac989137571a89c0cdbb7d03
Kamila1625 <kamila.de.bua.tresy@gmail.com>
2017-07-31 20:36:29 +0300

    Some UI

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QMenuBar>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
   MainWindow w;
-  
+  //w.show();
   w.showMaximized();
   
   return a.exec();
